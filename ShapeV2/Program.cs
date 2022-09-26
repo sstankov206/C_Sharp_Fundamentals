f4c5e6defa677e8753302392c66e6f91e5d34b4e
sstankov206 <stankovst@gmail.com>
2022-09-25 20:09:37 -0700

    vendingmachine

---
+++ ShapeV2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+
+static void Main()
+{
+    Console.WriteLine("Let's draw a shape! Select a shape by typing: 'triangle', 'square', 'rectangle'.");
+    string shape = Console.ReadLine();
+
+    if (shape == "triangle")
+    {
+
+        Console.WriteLine("    /|");
+        Console.WriteLine("   / |");
+        Console.WriteLine("  /  |");
+        Console.WriteLine(" /___|");
+
+        Console.WriteLine("You selected TRIANGLE");
+    }
+    else if (shape == "rectangle")
+    {
+
+        Console.WriteLine("    ____________ ");
+        Console.WriteLine("   |            |");
+        Console.WriteLine("   |            |");
+        Console.WriteLine("   |____________|");
+
+        Console.WriteLine("You selected RECTANGLE");
+    }
+    else if (shape == "square")
+    {
+
+        for (int i = 0; i <= 3; i++)
+        {
+
+
+            for (int x = 0; x <= 3; x++)
+
+            {
+                if (i == 0 || i == 3)
+                {
+                    Console.Write("_");
+                }
+                else if (x == 0 || x == 3)
+                {
+                    Console.Write("|");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("    _______");
+        Console.WriteLine("   |       |");
+        Console.WriteLine("   |       |");
+        Console.WriteLine("   |_______|");
+
+        Console.WriteLine("You selected SQUARE");
+    }
+    else
+    {
+        Console.WriteLine("Invalid selection. Please try again.");
+    }
+}
+Main();
--- VendingMachine/VendingMachine/Program.cs
+++ VendingMachine/VendingMachine/Program.cs
@@ -15,58 +15,82 @@ solution by me, sstankov */
 
 using System;
 
-namespace VendingMachine
+
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        //Calculating total money being used:
+
+        string command = Console.ReadLine();
+
+        double totalSum = 0;
+
+        while (command != "Start")
         {
-            //Calculating total money being used:
+            double currentAmount = double.Parse(command);
 
-            string command = Console.ReadLine();
-
-            double totalSum = 0;
-
-            while (command != "Start")
+            if (currentAmount != 0.1 && currentAmount != 0.20 && currentAmount != 0.5
+                && currentAmount != 1 && currentAmount != 2)
             {
-                double currentAmount = double.Parse(command);
-
-                if (currentAmount != 0.1 && currentAmount != 0.20 && currentAmount != 0.5
-                    && currentAmount != 1 && currentAmount != 2)
-                {
-                    Console.WriteLine($"Cannot accept {currentAmount},");
-                }
-                else
-                {
-                    totalSum += currentAmount;
-                }
-
-
+                Console.WriteLine($"Cannot accept {currentAmount}");
+            }
+            else
+            {
                 totalSum += currentAmount;
-                command = Console.ReadLine();
             }
 
-            // Buying products:
 
-            string input = Console.ReadLine();
-
-            while (input != "End")
-            {
-                double currentPrice = 0;
-                switch (input)
-                {
-                    case "Nuts":
-                        currentPrice = 2.0;
-                        break;
-                    case "Water":
-                        currentPrice = 0.7;
-                        break;
-                    default:
-                        break;
-
-                }
-            }
+            totalSum += currentAmount;
+            command = Console.ReadLine();
         }
+
+        // Buying products:
+
+        string item = Console.ReadLine();
+
+        while (item != "End")
+        {
+            double currentPrice = 0;
+            switch (item)
+            {
+                case "Nuts":
+                    currentPrice = 2.0;
+                    break;
+                case "Water":
+                    currentPrice = 0.7;
+                    break;
+                case "Crisps":
+                    currentPrice = 1.5;
+                    break;
+                case "Soda":
+                    currentPrice = 0.8;
+                    break;
+                case "Coke":
+                    currentPrice = 1.0;
+                    break;
+                default:
+                    Console.WriteLine("Invalid product");
+                    item = Console.ReadLine();
+                    continue;
+
+            }
+
+            if (totalSum >= currentPrice)
+            {
+                totalSum -= currentPrice;
+                Console.WriteLine($"Purchased {item}"); //where 'item' = product
+            }
+            else
+            {
+                Console.WriteLine("Sorry, not enough money");
+            }
+
+            item = Console.ReadLine();
+
+        }
+
+        Console.WriteLine($"Change: {totalSum:f2}"); //where 'totalSum' = money left
     }
 }
 
