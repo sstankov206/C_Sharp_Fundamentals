5894581623b6aaaba86b4d17ca57406a81491bd2
sstankov206 <stankovst@gmail.com>
2022-09-30 19:01:34 -0700

    sumDigits

---
+++ sumDigits/Program.cs
@@ -0,0 +1,32 @@
+﻿/* You will be given a single integer. Your task is to find the sum of its
+digits.
+
+   Examples:
+
+   245678  = 32
+   97561 = 28
+   543 = 12, etc.
+
+   solution by me, sstankov */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int num = int.Parse(Console.ReadLine());
+
+        int sum = 0; //will be used to store the sum of the digits
+
+        while (num != 0)
+        {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        Console.WriteLine(sum);
+    }
+
+
+}
