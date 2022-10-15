d147991e17e7e54cc2f3540fede9fb586567ccd2
sstankov206 <stankovst@gmail.com>
2022-10-14 23:27:44 -0700

    sumOfEvenNumbersArray

---
+++ sumOfEvenNumbers/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Read an array from the console and sum only the even numbers.
+*/
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //variable 'input' will store the numbers entered via console/input
+        var input = Console.ReadLine();
+
+        //numbers from 'input' will go in an array 'strings' and be split by a space
+        string[] strings = input.Split(" ");
+        
+
+        //content from 'strings' array go in a new array 'numbers' which is integers based
+        int[] numbers = new int[strings.Length];
+
+        for (int i = 0; i < strings.Length; i++)
+        {
+            numbers[i] = int.Parse(strings[i]);
+        }
+
+        // 'sum' will store the total sum
+        var sum = 0;
+        for (int i = 0; i < numbers.Length; i++) 
+        {
+            if (numbers[i] % 2 == 0) //check for even numbers
+            {
+                sum = sum + numbers[i]; //adds them to the sum
+            }
+        }
+        Console.WriteLine(sum); //output
+    }
+}
