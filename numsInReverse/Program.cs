a7507ecd13ad5f112d7229c9178b5ad2cc45788c
sstankov206 <stankovst@gmail.com>
2022-10-14 23:26:54 -0700

    numsInReverseArray

---
+++ numsInReverse/Program.cs
@@ -0,0 +1,47 @@
+﻿/* 
+  Read 'n' numbers and print them in reverse order.
+
+  solution by me, sstankov.
+ */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //reading 'n' from input:
+        int n = GetNumber();
+
+        //creating the array with size of 'n':
+        int[] numbers = new int[n];
+
+        //setting 'number' to the corresponding index:
+        for (int i = 0; i < n; i++)
+        {
+            int number = GetNumber();
+
+
+            numbers[i] = number;
+        }
+        
+        //printing the array in reverse order:
+        for (int j = numbers.Length - 1; j >= 0; j--)
+        {
+            Print(numbers[j] + " "); //output
+        }
+
+    }
+
+    public static int GetNumber()
+    {
+        var number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    public static void Print(string value)
+    {
+        Console.WriteLine(value);
+    }
+
+}
