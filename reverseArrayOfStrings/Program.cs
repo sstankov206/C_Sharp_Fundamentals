ffba7d08e5025b7dda518b4f5216c7813b4ad310
sstankov206 <stankovst@gmail.com>
2022-10-15 15:30:13 -0700

    reversingArrayOfStrings

---
+++ reverseArrayOfStrings/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Read an array of strings (space separated values), reverse it and print its elements:
+ * 
+ * Examples:
+ * - Input: -1 hi ho w
+ * - Output: w ho hi -1
+ *
+ *solution by me, sstankov
+ *
+ */
+
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //creating a string 'input' which will store elements of the array:
+        string input = Console.ReadLine(); //Ex: 1 2 3 4 5
+
+        //splits elements of the 'input' string by space & puts them in array:
+        var array = input.Split();
+
+        //reversing the array:
+        for (int index = array.Length - 1; index >= 0; index--)
+        {
+            Console.Write(array[index] + " "); //ouput, seperated with space.
+        }
+    }
+}
