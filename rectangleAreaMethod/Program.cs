ad05b9dd2434209591b36db1e250d942df13e360
sstankov206 <stankovst@gmail.com>
2022-10-26 17:16:09 -0700

    rectangleAreaMethods

---
+++ rectangleAreaMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿/* Create a method which returns rectangle area with given width and height
+ * 
+ * solution by me, sstankov
+ */
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        //invokes the method 'CalculateRectangleArea' to print area of
+        //rectangle based on numbers provided from input
+
+        Console.WriteLine(
+            CalculateRectangleArea
+            (int.Parse(Console.ReadLine()),
+            int.Parse(Console.ReadLine())));
+    }
+
+    static int CalculateRectangleArea(int a, int b)
+    {
+        return a * b; //prints the area of the rectangle
+    }
+}
