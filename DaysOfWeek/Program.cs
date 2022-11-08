030529d61ad8a1546af3557d4622a6efca1ac879
sstankov206 <stankovst@gmail.com>
2022-11-08 11:38:25 -0800

    DaysOfWeekObjects&Classes

---
+++ DaysOfWeek/Program.cs
@@ -0,0 +1,30 @@
+﻿/* You are given a date in format day-month-year
+ * Calculate and print the day of week in English
+ * 
+ * Examples:
+ * 
+ * 18-04-2016 -> Monday
+ * 27-11-1996 -> Wednesday
+ * 
+ * solution by me, sstankov.
+ */
+
+using System;
+using System.Text;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        //reading date in d-M-yyyy format, from console input:
+        string dateString = Console.ReadLine();
+
+        DateTime date = DateTime.ParseExact(dateString, "d-M-yyyy",
+            CultureInfo.InvariantCulture);
+
+        Console.WriteLine(date.DayOfWeek);
+    }
+}
+
+
